6001268fd7ed97b18a9683617036dd12a1895ca4
przemek milke <theblimek@gmail.com>
2024-03-09 10:56:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę :");
+            int liczba = Int32.Parse(Console.ReadLine());
+            if (liczba > 0)
+            {
+
+                Console.WriteLine("Podana liczba jest liczbą dodatnią ");
+
+            }
+
+            else
+            {
+                Console.WriteLine("podana licznba jest liczbą ujemną ");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
